9df565ad1322a58947ad5b3a032c1337128c315c
Thiago Marcelino | LAFORT <thiago.marcelino@lafortmalhas.local>
2025-08-14 11:27:44 -0300

    refactor: filtros por categoria e disponibilidade

--- app/metodoFilter.js
+++ app/metodoFilter.js
@@ -1,10 +1,30 @@
 const botoes = document.querySelectorAll(".btn");
 botoes.forEach(btn => btn.addEventListener("click", filtrarLivros));
 
-function filtrarLivros(){
+function filtrarLivros() {
     const elementoBtn = document.getElementById(this.id);
     const categoria = elementoBtn.value;
-    let livrosFiltrados = categoria == "disponivel" ? livros.filter(livro => livro.quantidade > 0) : livros.filter(livro => livro.categoria == categoria);
+    let livrosFiltrados = categoria == "disponivel" ? filtrarPorDisponibilidade() : filtrarPoCategoria(categoria);
     exibirLivrosNaTela(livrosFiltrados);
-   
+
+    if (categoria == "disponivel") {
+        exibirValorTotalDosLivrosDisponiveis();
+    }
+}
+
+
+function filtrarPoCategoria(categoria) {
+    return livros.filter(livro => livro.categoria == categoria);
+}
+
+function filtrarPorDisponibilidade() {
+    return livros.filter(livro => livro.quantidade > 0);
+}
+
+function exibirValorTotalDosLivrosDisponiveis() {
+    valorDosLivrosDisponiveis.innerHTML = `
+        <div class="livros__disponiveis">
+        <p>Todos os livros disponíveis por R$ <span id="valor">299,00</span></p>
+        </div>
+        `;
 }
--- app/metodoForEach.js
+++ app/metodoForEach.js
@@ -1,7 +1,9 @@
 const elementoParaInserirLivros = document.getElementById("livros");
+const valorDosLivrosDisponiveis = document.getElementById("valor_total_livros_disponiveis");
 
 function exibirLivrosNaTela(listaDeLivros) {
     elementoParaInserirLivros.innerHTML = "";
+    valorDosLivrosDisponiveis.innerHTML = "";
 
     listaDeLivros.forEach(livro => {
         let disponibilidade = livro.quantidade > 0 ? "livro__imagens" : "livro__imagens indisponivel";
